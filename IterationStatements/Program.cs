560e7707743def2df8d9c65fa77ffb33ca2fa709
Parker Sims <parker4al@gmail.com>
2025-04-10 15:26:00 -0500

    completed exercise 2

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -18,7 +18,7 @@ namespace IterationStatements
             //TODO - Create a variable of type int and name it "num"
             //TODO - Initialize the variable with a value of 0
 
-
+            int num = 0;
 
             //TODO - Create a do-while loop using the existing brackets as a template.
             // The existing brackets are placeholders for your loop code.
@@ -26,11 +26,13 @@ namespace IterationStatements
             // a) Increment "num" by 1
             // b) Then add "num" to the collection "numbers"
             //    Hint: Use "numbers.Add(num);" to add the current value of "num" to the list.
-
+            do
             {
                 // Your code goes here
-                numbers.Add(/*num*/); //uncomment out `num` to add it to the collection "numbers"
-            } 
+                Console.WriteLine(num);
+                num++;
+                numbers.Add(num); //uncomment out `num` to add it to the collection "numbers"
+            } while (num < 100);
             //TODO - Continue the loop while "num" is less than 100
             
 
@@ -41,9 +43,12 @@ namespace IterationStatements
             // a) Increment "num" by 1
             // b) Then add "num" to the collection "numbers"
             //    Hint: You can copy how this was done in the do-while loop
-
+            while (num < 200)
             {
                 // Your code goes here
+                Console.WriteLine(num);
+                num++;
+                numbers.Add(num);
             }
             
 
@@ -55,8 +60,10 @@ namespace IterationStatements
             //TODO - Create a foreach loop using the existing brackets as a template.
             // The existing brackets are placeholders for your loop code.
             // Inside this loop, print each number in "numbers".
+            foreach (int num2 in numbers)
             {
                 // Your code goes here
+                Console.WriteLine(num2);
             }
              
 
@@ -75,6 +82,7 @@ namespace IterationStatements
             //    - Greater than or equal to 0
             // c) Decrement "i" by 1 in each iteration
 
+            for (int i = 199; i >= 0; i--)
             {
                 //TODO - Inside the loop, place numbers[i] inside of the Console.WriteLine() method
                 Console.WriteLine(numbers[i]); // Example placement of numbers[i] inside Console.WriteLine
